a9c4e903ba146b2cbba3dc9e2b748a76afbb56bc
Doni Rubiagatra <doni.rubiagatra@kumparan.com>
2019-04-13 11:41:08 +0700

    Add login feature

--- kumparan/apps/authentication/views.py
+++ kumparan/apps/authentication/views.py
@@ -3,12 +3,14 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import RegistrationSerializer
+from .serializers import (
+    LoginSerializer, RegistrationSerializer
+)
 from .renderers import UserJSONRenderer
 
 
 class RegistrationAPIView(APIView):
-    # ALlow any user (authenticated or not) to hit this endpoint
+    # Allow any user (authenticated or not) to hit this endpoint
     permission_classes = (AllowAny,)
     renderer_classes = (UserJSONRenderer,)
     serializer_class = RegistrationSerializer
@@ -25,3 +27,20 @@ class RegistrationAPIView(APIView):
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     
+
+class LoginAPIView(APIView):
+    permission_classes = (AllowAny,)
+    renderer_classes = (UserJSONRenderer,)
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        user  = request.data.get('user', {})
+
+        # Notice here that we do not call `serializer.save()` like we did for
+        # the registration endpoint. This is because we don't have
+        # anything to save. Instead, the `validate` method on our serializer
+        # handles everything we need.
+        serializer = self.serializer_class(data=user)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
